3d5c734fcd752aad5413d99404462a20cc598621
Maria V <mariatua2@gmail.com>
2019-05-01 20:13:36 -0400

    update

--- src/laboratorio_1/Laboratorio_1.java
+++ src/laboratorio_1/Laboratorio_1.java
@@ -8,144 +8,120 @@ import java.util.Scanner;
  */
 
 public class Laboratorio_1 {
-    
-    public static void msjEdad(boolean val){
-       
-       if(val)
-            System.out.println("Es mayor de Edad");
-        else
-            System.out.println("No es mayor de Edad"); 
-    }
-    
+     
     public static void msjPeso(int val){
-    
-        if(val == 0)
-            System.out.println("Tiene peso ideal");
-        else if(val == -1)
-            System.out.println("Tiene bajo peso");
-        else if(val == 1)
-            System.out.println("Tiene SobrePeso");
+        if(val == -1)
+            System.out.println("Está muy delgada");
+        else if(val == 0)
+            System.out.println("Tiene exceso de grasa corporal");
         else
-            System.out.println("Tiene obesidad");
-
+            System.out.println("Tiene peso ideal");
     }
     
     public static void msjRitmoCard(boolean value) {
+      
         if (value)
-            System.out.println("Puedes hacer ejercicios");
+            System.out.println("El deportista puede hacer ejercicios");
         else
-            System.out.println("NO PUEDES HACER EJERCICIO"); 
+            System.out.println("Debido a su condición física, no puede realizar ejercicios"); 
     
     }
     
-    public static void main(String[] args) {
+    public static void llenarVarGlobales(Persona per, Scanner read, String nombre, int edad, char sexo, float peso, float altura, float ritmoCard, String frecEntr, String tipoEjer, int experiencia, String especialidad){
         
-//        /*Objeto con valores por default*/
-//        Persona pDef = new Persona();
-//        
-//        /*Asignacion de nuevos valores , usando setters, al objeto con valores default*/
-//        pDef.setAltura(2.00f);
-//        pDef.setCedula(12345678);
-//        pDef.setDireccion("St. Street none of your business Apt #4");
-//        pDef.setEdad(27);
-//        pDef.setNombre("Abraham Duke");
-//        pDef.setPeso(90);
-//        pDef.setSexo('M');
-//        
-//        System.out.println("<--- Objeto Default --->");
-//        msjEdad(pDef.esMayorDeEdad());
-//        msjPeso(pDef.calcularIMC());
-//        pDef.imprimirDatosPer();
-//        
-//        
-        /*Inputs del segundo Objeto*/
-        Scanner read = new Scanner( System.in );
-        
-//        System.out.println("Inserte nombre: ");
-//        String nombre = read.nextLine();
-//        
-//        System.out.println("Inserte edad: ");
-//        int edad = read.nextInt();
-//        
-//        char sexo;
-//        do{
-//            System.out.println("Inserte sexo: ");
-//            sexo = read.next().charAt(0);
-//        }while(!pDef.comprobarSexo(sexo));
-//        
-//        System.out.println("Inserte peso: ");
-//        float peso = read.nextFloat();
-//        
-//        System.out.println("Inserte altura: ");
-//        float altura = read.nextFloat();
-//        
-//        /*Objeto con valores del Input*/
-//        Persona pIn = new Persona(0, nombre, edad, sexo, peso, altura, "");
-//        
-//        System.out.println("<--- Objeto Input --->");
-//        msjEdad(pIn.esMayorDeEdad());
-//        msjPeso(pIn.calcularIMC());
-//        pIn.imprimirDatosPer();
-        
-            //
-            Deportista depor1= new Deportista();
-            
-            System.out.println("Inserte nombre: ");
-            String nombre = read.nextLine();
+           System.out.println("Inserte nombre: ");
+            nombre = read.nextLine();
 
             System.out.println("Inserte edad: ");
-            int edad = read.nextInt();
+            edad = read.nextInt();
 
-            char sexo;
+            
             do{
-                System.out.println("Inserte sexo: ");
+                System.out.println("Inserte sexo (M o F): ");
                 sexo = read.next().charAt(0);
-            }while(!depor1.comprobarSexo(sexo));
+            }while(!per.comprobarSexo(sexo));
 
             System.out.println("Inserte peso: ");
-            float peso = read.nextFloat();
+            peso = read.nextFloat();
 
             System.out.println("Inserte altura: ");
-            float altura = read.nextFloat();
+            altura = read.nextFloat();
             
             System.out.println("Inserte Ritmo Cardiaco");
-            float ritmoCard = read.nextFloat();
+            ritmoCard = read.nextFloat();
             
             read.nextLine();
             
-            String frecEntr;
+            
             do{
-                System.out.println("Indique la frecuencia de entrenamieno: ");
-                frecEntr = read.nextLine();
-                System.out.println(frecEntr);
-            }while(frecEntr.compareToIgnoreCase("diario")!= 0 );
+                System.out.println("Indique la frecuencia de entrenamiento (diario o semanal): ");
+                frecEntr = read.nextLine();   
+     
+            } while(!frecEntr.equals("diario") && !frecEntr.equals("semanal"));
             
            
+            /*TODO switch tipo de ejercicio*/
             System.out.println("Indique el tipo de ejercicio que realiza");
-            String tipoEjer = read.nextLine().toLowerCase();
+            tipoEjer = read.nextLine().toLowerCase();
             
             
-            Deportista depor2= new Deportista(ritmoCard, frecEntr, tipoEjer, 0 , nombre,edad, sexo, peso, altura, "");
+            if(per instanceof Entrenador){
+                System.out.println("Indica tus años de experiencia: ");
+                experiencia = read.nextInt();
             
-            msjRitmoCard(depor2.calcRitmoCard());
+                read.nextLine();
             
-            Entrenador entre1 = new Entrenador();
+                System.out.println("Indica tu especialidad ");
+                especialidad = read.nextLine();
             
-            System.out.println("Indica tus años de experiencia: ");
-            int experiencia = read.nextInt();
-            System.out.println("Indica tu especialidad ");
-            String especialidad = read.nextLine();
-            
-            Entrenador entre2 = new Entrenador(experiencia,especialidad, 0 , nombre,edad, sexo, peso, altura, "");
-           
-            System.out.println("Rutina 1");
-            entre2.determinarRutina(depor2.getTipoEj(), depor2.calcularIMC());
-            System.out.println("Rutina 2");
-            entre2.determinarRutina(depor2.calcularIMC(), depor2.getTipoEj());
+            }
+                
+        
+    }
+    
+    public static void main(String[] args) {
+    
+         
+        String nombre="";
+        int edad=0;
+        char sexo=' ';
+        float peso=0;
+        float altura=0;
+        float ritmoCard=0;
+        String frecEntr="";
+        String tipoEjer="";
+        int experiencia=0;
+        String especialidad="";
+      
+        /*Instancia de la clase scanner*/
+        Scanner read = new Scanner( System.in );
+        
+        Deportista depor1= new Deportista();
+        Entrenador entre1 = new Entrenador();
+        
+        llenarVarGlobales(depor1,read,nombre,edad,sexo,peso,altura,ritmoCard,frecEntr,tipoEjer,experiencia,especialidad);
+        
+        System.out.println("Rellene los datos del deportista 2: ");
+        Deportista depor2= new Deportista(ritmoCard, frecEntr, tipoEjer, 0 , nombre,edad, sexo, peso, altura, "");
+        
+        /*Chequear si puede hacer ejercicio*/
+        msjRitmoCard(depor2.calcRitmoCard());
+        
+        /*Revisar grasa corporal*/
+        msjPeso(depor2.calcularIMC());
+        
+        System.out.println("Rellene los datos del entrenador 2:");
+        llenarVarGlobales(entre1,read,nombre,edad,sexo,peso,altura,ritmoCard,frecEntr,tipoEjer,experiencia,especialidad);     
+        Entrenador entre2 = new Entrenador(experiencia,especialidad, 0 , nombre,edad, sexo, peso, altura, "");
+        
+        System.out.println("Rutinas recomendadas al deportista por parte del entrenador:");
+        System.out.println("Rutina 1:");
+        entre2.determinarRutina(depor2.getTipoEj(), depor2.calcularIMC());
+        System.out.println("Rutina 2:");
+        entre2.determinarRutina(depor2.calcularIMC(), depor2.getTipoEj());
             
         
-        
-        
+            
     }
     
     
